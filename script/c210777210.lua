888d4c748108876e13df48cc8fa3709f81631a57
NaimSantos <naimsantos45@gmail.com>
2020-06-11 11:35:34 -0300

    Added "Vylon Diode"

---
+++ script/c210777210.lua
@@ -0,0 +1,40 @@
+--Vylon Diode
+--designed and scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddUnionProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x30),nil,true)
+	--Destroy and Special Summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND+LOCATION_GRAVE)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+end
+function s.filter(c,ft,check)
+	return c:IsFaceup() and ((c:IsType(TYPE_EQUIP) and ft>0)
+		or (check and c:IsSetCard(0x30) and (ft>0 or (c:IsLocation(LOCATION_MZONE) and c:GetSequence()<5))))
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local ft,loc=Duel.GetLocationCount(tp,LOCATION_MZONE),LOCATION_SZONE
+	local check=Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsType,TYPE_SYNCHRO),tp,LOCATION_MZONE,0,1,nil)
+	if check then loc=LOCATION_ONFIELD end
+	if chkc then return chkc:IsLocation(loc) and chkc:IsControler(tp) and s.filter(chkc,ft,check) end
+	if chk==0 then return ft>-1 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.IsExistingTarget(s.filter,tp,loc,0,1,nil,ft,check) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,s.filter,tp,loc,0,1,1,nil,ft,check)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 and c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
