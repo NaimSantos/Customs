c511f32809eca1031ce75ca775afcd3c10dca68d
NaimSantos <naimsantos45@gmail.com>
2020-07-21 15:05:35 -0300

    Add "Litmus Doom Source"

--- script/c210777220.lua
+++ script/c210777220.lua
@@ -1,4 +1,48 @@
 --Litmus Doom Source
-function c210777220.initial_effect(c)
-	
+--designed and scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	Ritual.AddProcEqual(c,s.ritualfil,nil,nil,nil,nil,nil,nil,LOCATION_HAND|LOCATION_GRAVE)
+	--Return to hand and Draw
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(s.thcost)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+end
+s.listed_names={72566043}
+function s.ritualfil(c)
+	return c:IsRace(RACE_WARRIOR) and c:IsRitualMonster()
+end
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToDeckAsCost() end
+	Duel.SendtoDeck(e:GetHandler(),nil,2,REASON_COST)
+end
+function s.thfilter(c)
+	return c:IsCode(72566043) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		if Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 then
+			Duel.ConfirmCards(1-tp,tc)
+			if Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil,TYPE_TRAP)
+				and Duel.IsPlayerCanDraw(tp,1) and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+					Duel.BreakEffect()
+					Duel.Draw(tp,1,REASON_EFFECT)
+				end
+		end
+	end
 end
