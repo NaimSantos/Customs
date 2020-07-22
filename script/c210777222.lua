12745019169031d6b2625b2da105b458c641351b
NaimSantos <naimsantos45@gmail.com>
2020-07-22 09:35:27 -0300

    Add "Litmus Doom Knight"

--- script/c210777222.lua
+++ script/c210777222.lua
@@ -1,4 +1,96 @@
 --Litmus Doom Knight
-function c210777222.initial_effect(c)
-	
+--designed and scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Position change
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e1:SetTarget(s.postg)
+	e1:SetOperation(s.posop)
+	c:RegisterEffect(e1)
+	--Special Summon from hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DRAW)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_RELEASE)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.drcond)
+	e2:SetTarget(s.drtg)
+	e2:SetOperation(s.drop)
+	c:RegisterEffect(e2)
+	--Level change
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetCode(EFFECT_CHANGE_LEVEL)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(s.lvcond)
+	e3:SetValue(8)
+	c:RegisterEffect(e3)
+end
+function s.tdfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_TRAP) and c:IsType(TYPE_CONTINUOUS)
+end
+function s.postg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_SZONE,0,1,nil)
+		and Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g1=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+	e:SetLabelObject(g1)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g2=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_SZONE,0,1,1,nil)
+	g2:Merge(g1)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g2,2,0,0)
+end
+function s.posop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local tg=g:Filter(Card.IsRelateToEffect,nil,e)
+	if #tg>0 then
+		local tc=tg:GetFirst() --spell/trap
+		if tc==e:GetLabelObject() then tc=tg:GetNext() end --se for o monstro
+		if e:GetLabelObject():IsLocation(LOCATION_MZONE) and e:GetLabelObject():IsFaceup() then
+			Duel.ChangePosition(e:GetLabelObject(),POS_FACEDOWN_DEFENSE)
+		end
+		if tc:IsLocation(LOCATION_SZONE) and tc:IsFaceup() and tc:IsSSetable(true) then
+			tc:CancelToGrave()
+			Duel.ChangePosition(tc,POS_FACEDOWN)
+			tc:SetStatus(STATUS_SET_TURN,false)
+			Duel.RaiseEvent(tc,EVENT_SSET,e,REASON_EFFECT,tp,tp,0)
+	end
+end
+function s.drcond(e,tp,eg,ep,ev,re,r,rp)
+	return (r&REASON_EFFECT)~=0
+end
+function s.filter(c)
+	return c:IsType(TYPE_TRAP) and c:IsType(TYPE_CONTINUOUS) and c:IsAbleToDeck()
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and chkc:IsControler(tp) and s.filter(chkc) end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2)
+		and Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,3,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local td=tg:Filter(Card.IsRelateToEffect,nil,e)
+	if not tg or #td<=0 then return end
+	Duel.SendtoDeck(td,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	local g=Duel.GetOperatedGroup()
+	if #g>0 then
+		Duel.BreakEffect()
+		Duel.Draw(tp,2,REASON_EFFECT)
+	end
 end
