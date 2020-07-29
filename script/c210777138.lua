3744003923a71970a318e332eb4262597df0dd66
NaimSantos <naimsantos45@gmail.com>
2020-07-29 08:39:25 -0300

    Add  "Idol, the Melodious Diva"

---
+++ script/c210777138.lua
@@ -0,0 +1,49 @@
+--Idol, the Melodious Diva
+--designed and scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Link Summon Procedure
+	Link.AddProcedure(c,s.matfilter,1,1)
+	c:EnableReviveLimit()
+	--ATK increase
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(s.atkval)
+	c:RegisterEffect(e1)
+	--Special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCountLimit(1)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.matfilter(c,lc,sumtype,tp)
+	return c:IsSetCard(0x9b,lc,sumtype,tp) and not c:IsSummonCode(lc,sumtype,tp,id)
+end
+function s.atkval(e,c)
+	local g=e:GetHandler():GetLinkedGroup():Filter(Card.IsFaceup,nil) 
+	return g:GetSum(Card.GetBaseAttack)
+end
+function s.filter(c,e,tp)
+	return c:IsSetCard(0x9b) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	end
+end
