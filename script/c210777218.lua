6ab81820d2ba4147d4bf56bd66399269466da51c
NaimSantos <naimsantos45@gmail.com>
2020-08-02 10:06:06 -0300

    Add "Fortune Lady Early"

---
+++ script/c210777218.lua
@@ -0,0 +1,63 @@
+--Fortune Lady Early
+--designed and scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Change ATK/DEF
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_SET_ATTACK)
+	e1:SetValue(s.value)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_SET_DEFENSE)
+	c:RegisterEffect(e2)
+	--Increase Level in the Standby Phase
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCategory(CATEGORY_REMOVE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e3:SetCondition(s.lvcon)
+	e3:SetOperation(s.lvop)
+	c:RegisterEffect(e3)
+end
+function s.value(e,c)
+	return c:GetLevel()*400
+end
+function s.lvcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp and e:GetHandler():IsLevelAbove(1) and e:GetHandler():IsLevelBelow(11)
+end
+function s.rmvfilter(c)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToRemove()
+end
+function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local g=Duel.GetMatchingGroup(s.rmvfilter,tp,0,LOCATION_ONFIELD,nil)
+	if #g>0 then
+		Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+	end
+end
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local lvl=c:GetLevel()
+	if c:IsFacedown() or not c:IsRelateToEffect(e) or c:IsLevelAbove(12) then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_LEVEL)
+	e1:SetValue(1)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+	c:RegisterEffect(e1)
+	local g=Duel.GetMatchingGroup(s.rmvfilter,tp,0,LOCATION_ONFIELD,nil)
+	if c:GetLevel()==lvl+1 and #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local rg=g:Select(tp,1,1,nil)
+		if #rg>0 then
+			Duel.BreakEffect()
+			Duel.Remove(rg,POS_FACEUP,REASON_EFFECT)
+		end
+	end
+end
