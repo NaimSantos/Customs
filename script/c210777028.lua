080a088e65ef91209db3296e245636488f320c60
NaimSantos <naimsantos45@gmail.com>
2021-01-08 18:55:00 -0300

    Scripts para cartas feitas para o G2Pro

--- script/c210777028.lua
+++ script/c210777028.lua
@@ -1,51 +1,49 @@
---Magician's Mind
---Scripted and designed by Naim
-local s,id=GetID()
-function s.initial_effect(c)
+--Call of Timaeus
+--designed by Xeno Disturbia#5235, scripted by Naim
+function c210777028.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,210777028)
+	e1:SetCost(c210777028.cost)
+	e1:SetTarget(c210777028.target)
+	e1:SetOperation(c210777028.activate)
 	c:RegisterEffect(e1)
-	--negate monster
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_CHAIN_SOLVING)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetCountLimit(1)
-	e2:SetCondition(s.negcon)
-	e2:SetOperation(s.negop)
-	c:RegisterEffect(e2)
-	--recover LP
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetCategory(CATEGORY_RECOVER)
-	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e3:SetCode(EVENT_LEAVE_FIELD)
-	e3:SetTarget(s.recovertg)
-	e3:SetOperation(s.recoverop)
-	c:RegisterEffect(e3)
 end
-function s.cfilter(c)
-	return c:IsFaceup() and c:IsRace(RACE_SPELLCASTER)
+c210777028.listed_names={46986414,38033121,1784686}
+function c210777028.timfilter(c)
+	return c:IsCode(1784686) and c:IsAbleToHand()
 end
-function s.negcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
-		and rp~=tp and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainDisablable(ev) 
+function c210777028.mfilter(c)
+	return c:IsCode(46986414,38033121) and c:IsAbleToHand()
 end
-function s.negop(e,tp,eg,ep,ev,re,r,rp)
-	local rc=re:GetHandler()
-	if Duel.NegateEffect(ev) and rc:IsRelateToEffect(re) then
-		Duel.Destroy(rc,REASON_EFFECT)
+function c210777028.exfilter(c,tp)
+	return c:IsCode(75380687,43892408) --cards that can only be summoned with Timaeus
+		and Duel.IsExistingMatchingCard(c210777028.mfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
+		and Duel.IsExistingMatchingCard(c210777028.timfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
+end
+function c210777028.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) 
+		and Duel.IsExistingMatchingCard(c210777028.exfilter,tp,LOCATION_EXTRA,0,1,nil,tp) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local g=Duel.SelectMatchingCard(tp,c210777028.exfilter,tp,LOCATION_EXTRA,0,1,1,nil,tp)
+	Duel.ConfirmCards(1-tp,g)
+	e:SetLabel(g:GetFirst())
+end
+function c210777028.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c210777028.timfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+function c210777028.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c210777028.timfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	if g:GetCount()>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
+		Duel.ConfirmCards(1-tp,g)
+		local cg=Duel.SelectMatchingCard(tp,c210777028.mfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+		Duel.SendtoHand(cg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,cg)
 	end
-end
-function s.recovertg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(400)
-	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,400)
-end
-function s.recoverop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Recover(tp,400,REASON_EFFECT)
 end
--- script/c210777029.lua
+++ script/c210777029.lua
@@ -1,55 +1,61 @@
---Magician's Reroll
---Scripted and designed by Naim
-local s,id=GetID()
-function s.initial_effect(c)
-	--search
+--Call of Hermos
+--designed by Xeno Disturbia#5235
+--scripted by Naim, credits to Larry126 for the fixes
+function c210777029.initial_effect(c)
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetCountLimit(1,210777029)
+	e1:SetCost(c210777029.cost)
+	e1:SetTarget(c210777029.target)
+	e1:SetOperation(c210777029.activate)
+	e1:SetLabel(0)
 	c:RegisterEffect(e1)
-	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_TOHAND)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCost(aux.bfgcost)
-	e2:SetTarget(s.thtg)
-	e2:SetOperation(s.thop)
-	e2:SetCountLimit(id,1)
-	c:RegisterEffect(e2)
 end
-s.listed_series={0x9f,0x98}
-function s.filter(c)
-	return c:IsSetCard(0x9f) and c:IsType(TYPE_MONSTER) and (not c:IsType(TYPE_PENDULUM)) and c:IsAbleToHand()
+function c210777029.hermfilter(c) --this is hermos
+	return c:IsCode(46232525) and c:IsAbleToHand()
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+function c210777029.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(100)
+	if chk==0 then return true end
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
+function c210777029.tohandfilter(c,tc,e,tp) --the thing that should be added (this is a simplification)
+	return c:GetOriginalLevel()==tc:GetOriginalLevel()
+		and c:GetOriginalRace()==tc:GetOriginalRace()
+		and c:GetOriginalAttribute()==tc:GetOriginalAttribute()
+		and c:GetBaseAttack()==tc:GetBaseAttack()
+		and c:GetBaseDefense()==tc:GetBaseDefense()
+end
+function c210777029.exfilter(c,e,tp) --the thing in the ED that can be revealed
+	return c:IsCode(19747827,83743222,46354113,10960419) and c:IsLocation(LOCATION_EXTRA)--cards that can only be summoned with Hermos
+		and Duel.IsExistingMatchingCard(c210777029.tohandfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,c,e,tp)
+end
+function c210777029.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		if e:GetLabel()~=100 then return false end
+		e:SetLabel(0)
+		return e:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsExistingMatchingCard(c210777029.exfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) 
+		and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler())
+		and Duel.IsExistingMatchingCard(c210777029.hermfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
+	end
+	e:SetLabel(0)
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK+LOCATION_GRAVE)
+	local g=Duel.SelectMatchingCard(tp,c210777029.exfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	Duel.ConfirmCards(1-tp,g)
+	e:SetLabelObject(g:GetFirst())
+	Duel.SetTargetCard(g)
+end
+function c210777029.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
-	if #g>0 then
+	tc=e:GetLabelObject()
+	local cg=Duel.SelectMatchingCard(tp,c210777029.tohandfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,tc,e,tp)
+	if cg:GetCount()>0 and Duel.SendtoHand(cg,nil,REASON_EFFECT)>0 then
+		Duel.ConfirmCards(1-tp,cg)
+		local g=Duel.SelectMatchingCard(tp,c210777029.hermfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
-	end
-end
-function s.thfilter(c)
-	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsSetCard(0x98) and c:IsAbleToHand()
-end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.thfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler(),nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
-end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 	end
 end
