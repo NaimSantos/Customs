8210249ea0c257a24fa01d516c20d39bc54167f0
NaimSantos <naimsantos45@gmail.com>
2022-03-22 08:45:02 -0300

    Added effct to "Performapal Blazing Magician"

--- script/c210777030.lua
+++ script/c210777030.lua
@@ -15,18 +15,19 @@ function s.initial_effect(c)
 	e0:SetTarget(s.edsptg)
 	e0:SetOperation(s.edspop)
 	c:RegisterEffect(e0)
-	--Return 1 Spell to hand and activate 1 Continuous Spell
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,1))
-	e1:SetCategory(CATEGORY_TOHAND)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1)
-	e1:SetTarget(s.thtg)
-	e1:SetOperation(s.thop)
-	c:RegisterEffect(e1)
+	--Return 1 Spell to hand and destroy 1 card
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetHintTiming(TIMING_MAIN_END+TIMING_END_PHASE,TIMING_MAIN_END+TIMING_END_PHASE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
 end
 function s.ffilter(c,fc,sumtype,tp)
 	return c:IsSetCard(0x9f,fc,sumtype,tp) and not c:IsType(TYPE_PENDULUM,fc,sumtype,tp)
@@ -72,32 +73,27 @@ function s.edspop(e,tp,eg,ep,ev,re,r,rp,c)
 	g:DeleteGroup()
 end
 function s.thfilter(c)
-	return c:IsFaceup() and c:GetType()==TYPE_SPELL and c:IsAbleToHand()
+	return c:IsFaceup() and c:IsType(TYPE_SPELL) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_SZONE) and chkc:IsControler(tp) and s.thfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_SZONE,0,1,nil) end
+	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(tp) and s.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_ONFIELD,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_SZONE,0,1,1,nil)
+	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_ONFIELD,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_ONFIELD)
 end
-function s.tffilter(c,tp)
-	return c:IsSetCard(0x98) and c:GetType()==TYPE_SPELL+TYPE_CONTINUOUS and c:GetActivateEffect():IsActivatable(tp,true)
-end
+
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 then
-		local g=Duel.GetMatchingGroup(s.tffilter,tp,LOCATION_HAND,0,nil,tp)
-		if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+	if tc and tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 then
+		local dg=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+		if #dg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
 			Duel.BreakEffect()
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-			local sc=g:Select(tp,1,1,nil):GetFirst()
-			Duel.MoveToField(sc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
-			local te=sc:GetActivateEffect()
-			local tep=sc:GetControler()
-			local cost=te:GetCost()
-			if cost then cost(te,tep,eg,ep,ev,re,r,rp,1) end
-			Duel.RaiseEvent(sc,id,te,0,tp,tp,Duel.GetCurrentChain())
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+			local tc=dg:Select(tp,1,1,nil)
+			Duel.HintSelection(tc)
+			Duel.Destroy(tc,REASON_EFFECT)
 		end
 	end
 end
