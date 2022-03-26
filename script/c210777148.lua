3905f5e6e82bdd305c4afb7dc7e83768ff29f63a
NaimSantos <naimsantos45@gmail.com>
2022-03-26 09:43:38 -0300

    Finish "Walking Fossil"

--- script/c210777148.lua
+++ script/c210777148.lua
@@ -2,17 +2,92 @@
 --designed and scripted by Naim
 local s,id=GetID()
 function s.initial_effect(c)
-	--Prevent activations during the Battle Phase if banished
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCode(EVENT_REMOVE)
-	e3:SetCondition(s.actcon)
-	e3:SetOperation(s.actop)
+	--Add 1 "Fossil Fusion" to hand on Summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e3)
+	--Excavate and add to the hand
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,{id})
+	e4:SetTarget(s.thtg)
+	e4:SetOperation(s.thop)
+	c:RegisterEffect(e4)
+	--Prevent activations during the Battle Phase if banished
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,2))
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e5:SetProperty(EFFECT_FLAG_DELAY)
+	e5:SetCode(EVENT_REMOVE)
+	e5:SetCondition(s.actcon)
+	e5:SetOperation(s.actop)
+	c:RegisterEffect(e5)
 end
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+s.listed_names={CARD_FOSSIL_FUSION}
+function s.thfilter1(c)
+	return c:IsAbleToHand() and not c:IsCode(id) and (c:IsCode(CARD_FOSSIL_FUSION) or aux.IsCodeListed(c,CARD_FOSSIL_FUSION))
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter1,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter1,tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.thfilter2(c)
+	return c:aux.IsCodeListed(c,CARD_FOSSIL_FUSION) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=Duel.GetMatchingGroupCount(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
+	if chk==0 then
+		if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<ct then return false end
+		local g=Duel.GetDecktopGroup(tp,ct)
+		return g:FilterCount(Card.IsAbleToHand,nil)>0
+	end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	local ac=Duel.GetMatchingGroupCount(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
+	Duel.ConfirmDecktop(p,ac)
+	local g=Duel.GetDecktopGroup(p,ac)
+	if #g>0 and g:IsExists(s.thfilter2,1,nil) and Duel.SelectYesNo(p,aux.Stringid(id,3)) then
+		Duel.Hint(HINT_SELECTMSG,p,HINTMSG_ATOHAND)
+		local sg=g:FilterSelect(p,s.thfilter2,1,1,nil)
+		Duel.DisableShuffleCheck()
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-p,sg)
+		Duel.ShuffleHand(p)
+		ac=ac-1
+	end
+	if ac>0 then
+		Duel.MoveToDeckBottom(ac,tp)
+		Duel.SortDeckbottom(tp,tp,ac)
+	end
+end
+function s.actcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsPreviousLocation(LOCATION_GRAVE)
 end
 function s.actop(e,tp,eg,ep,ev,re,r,rp)
@@ -21,12 +96,12 @@ function s.actop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e1:SetTargetRange(0,1)
-	e1:SetCondition(s.actcon)
+	e1:SetCondition(s.phasecond)
 	e1:SetValue(1)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 end
-function s.actcon(e)
+function s.phasecond(e)
 	local ph=Duel.GetCurrentPhase()
 	return ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE
 end
