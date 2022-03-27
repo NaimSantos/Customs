d990bb8636ee3d4a0edf3f3a3752d96eb5192fd4
NaimSantos <naimsantos45@gmail.com>
2022-03-27 09:59:53 -0300

    Added "Temple of the Venomous Snakes" and Venom Token

---
+++ script/c210777161.lua
@@ -0,0 +1,140 @@
+--Temple of the Venomous Snakes
+--designed and scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Add counters during the End Phase
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_COUNTER)
+	e2:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
+	e2:SetCode(EVENT_PHASE+PHASE_END)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1)
+	e2:SetOperation(s.adctop)
+	c:RegisterEffect(e2)
+	--Increase ATK per Venom counter
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e3:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x50))
+	e3:SetValue(s.value)
+	c:RegisterEffect(e3)
+	--Increase DEF per Venom counter
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e4)
+	--Special Summon 2 tokens to your field
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,2))
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e5:SetType(EFFECT_TYPE_IGNITION)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetCountLimit(1)
+	e5:SetCost(s.spcost)
+	e5:SetTarget(s.sptg)
+	e5:SetOperation(s.spop)
+	c:RegisterEffect(e5)
+	--Move "Venom Swamp" to the field
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,3))
+	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e6:SetCode(EVENT_TO_GRAVE)
+	e6:SetCondition(s.tfcond)
+	e6:SetTarget(s.tftg)
+	e6:SetOperation(s.tfop)
+	c:RegisterEffect(e6)
+	local e7=e6:Clone()
+	e7:SetCode(EVENT_REMOVE)
+	c:RegisterEffect(e7)
+end
+s.listed_names={54306223}
+s.listed_series={0x50}
+s.counter_place_list={0x1009}
+function s.adctop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Group.CreateGroup()
+	local tg=Duel.GetFieldGroup(tp,LOCATION_MZONE,LOCATION_MZONE)
+	local tc=tg:GetFirst()
+	for tc in aux.Next(tg) do
+		if tc:IsCanAddCounter(0x1009,1) and not tc:IsSetCard(0x50) then
+			local atk=tc:GetAttack()
+			tc:AddCounter(0x1009,1)
+			if atk>0 and tc:GetAttack()==0 then
+				g:AddCard(tc)
+			end
+		end
+	end
+	if #g>0 then
+		Duel.RaiseEvent(g,EVENT_CUSTOM+id,e,0,0,0,0)
+	end
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0xe0)
+end
+function s.value(e,c)
+	return Duel.GetCounter(0,1,1,0x1009)*100
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsCanRemoveCounter(tp,1,1,0x1009,2,REASON_COST) end
+	Duel.RemoveCounter(tp,1,1,0x1009,2,REASON_COST)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,210777162,0X50,TYPES_TOKEN,1000,1000,4,RACE_REPTILE,ATTRIBUTE_EARTH) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,2,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) and Duel.GetLocationCount(tp,LOCATION_MZONE)>1 
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,210777162,0X50,TYPES_TOKEN,1000,1000,4,RACE_REPTILE,ATTRIBUTE_EARTH) then
+		local token1=Duel.CreateToken(tp,210777162)
+		Duel.SpecialSummonStep(token1,0,tp,tp,false,false,POS_FACEUP)
+		local token2=Duel.CreateToken(tp,210777162)
+		Duel.SpecialSummonStep(token2,0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummonComplete()
+	end
+end
+function s.tffilter(c,tp)
+	return c:IsCode(54306223) and not c:IsForbidden() and c:GetActivateEffect():IsActivatable(tp,true,true)
+end
+function s.tfcond(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.tftg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+end
+function s.tfop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local tc=Duel.SelectMatchingCard(tp,s.tffilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,tp):GetFirst()
+	if tc then
+		aux.PlayFieldSpell(tc,e,tp,eg,ep,ev,re,r,rp)
+		--Cannot be destroyed by opponent's card effects
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(aux.Stringid(id,4))
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		--Cannot be targeted or destroyed by card effects
+		local e2=Effect.CreateEffect(c)
+		e2:SetDescription(aux.Stringid(id,5))
+		e2:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+		e2:SetValue(1)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e2)
+	end
+end
