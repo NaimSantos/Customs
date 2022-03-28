7748b19263c79c57b0e2553973cbeca4c7b849f8
NaimSantos <naimsantos45@gmail.com>
2022-03-28 09:27:46 -0300

    Added script to "Polychimera Kraken"

--- script/c210777410.lua
+++ script/c210777410.lua
@@ -5,10 +5,73 @@ function s.initial_effect(c)
 	c:SetUniqueOnField(1,0,id)
 	c:EnableReviveLimit()
 	Fusion.AddProcMixN(c,true,true,s.ffilter,2)
+	--Destroy and Negate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DISABLE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1)
 end
 function s.ffilter(c,fc,sumtype,tp,sub,mg,sg)
 	return c:IsRace(RACE_FIEND,fc,sumtype,tp) and (not sg or sg:FilterCount(aux.TRUE,c)==0 or sg:IsExists(s.fusfilter,1,c,c:GetCode(fc,sumtype,tp),fc,tp))
 end
 function s.fusfilter(c,code,fc,tp)
 	return c:IsSummonCode(fc,SUMMON_TYPE_FUSION,tp,code) or c:IsHasEffect(511002961)
+end
+function s.desfilter(c,e,tp)
+	local g=Group.FromCards(e:GetHandler(),c)
+	return c:IsControler(tp)
+		and Duel.IsExistingTarget(aux.disfilter3,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,g)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,LOCATION_ONFIELD,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g1=Duel.SelectTarget(tp,s.desfilter,tp,LOCATION_ONFIELD,0,1,1,nil,e,tp)
+	e:SetLabelObject(g1:GetFirst())
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
+	local g=Group.FromCards(e:GetHandler(),g1:GetFirst())
+	local g2=Duel.SelectTarget(tp,aux.disfilter3,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,g)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g1,#g1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g2,#g2,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local dstg=e:GetLabelObject() --to destroy
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local ngtg=g:GetFirst() --to negate
+	if dstg==ngtg then
+		dstg=g:GetNext()
+	end
+	if Duel.Destroy(dstg,REASON_EFFECT)~=0 and ngtg and ((ngtg:IsFaceup() and not ngtg:IsDisabled()) or ngtg:IsType(TYPE_TRAPMONSTER)) and ngtg:IsRelateToEffect(e) then
+		Duel.NegateRelatedChain(ngtg,RESET_TURN_SET)
+		local c=e:GetHandler()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		ngtg:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetValue(RESET_TURN_SET)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		ngtg:RegisterEffect(e2)
+		if ngtg:IsType(TYPE_TRAPMONSTER) then
+			local e3=Effect.CreateEffect(c)
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
+			e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			ngtg:RegisterEffect(e3)
+		end
+	end
 end
