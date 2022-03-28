72f22bfa70b5ed29fa7f5d96a86bc2cfe9879cc8
NaimSantos <naimsantos45@gmail.com>
2022-03-28 14:08:26 -0300

    Added effect to "Polychimera Cerberus"

--- script/c210777413.lua
+++ script/c210777413.lua
@@ -5,10 +5,45 @@ function s.initial_effect(c)
 	c:SetUniqueOnField(1,0,id)
 	c:EnableReviveLimit()
 	Fusion.AddProcMixN(c,true,true,s.ffilter,3)
+	--Destroy 1 monster and Special Summon 1 Token
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_TOKEN)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetHintTiming(0,TIMING_MAIN_END+TIMING_SUMMON+TIMING_SPSUMMON)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
 end
 function s.ffilter(c,fc,sumtype,tp,sub,mg,sg)
 	return c:IsRace(RACE_FIEND,fc,sumtype,tp) and c:HasLevel() and (not sg or not sg:IsExists(s.fusfilter,1,c,c:GetLevel(fc,sumtype,tp),fc,sumtype,tp))
 end
 function s.fusfilter(c,lvl,fc,sumtype,tp)
 	return c:IsLevel(lvl) and not c:IsHasEffect(511002961)
+end
+function s.desfilter(c,ft,tp)
+	return ft>0 or (c:IsControler(tp) and c:GetSequence()<5)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_MZONE)
+	if chk==0 then
+		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+		return Duel.IsExistingMatchingCard(s.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,ft,tp)
+			and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0xf14,TYPES_TOKEN,1000,1000,8,RACE_FIEND,ATTRIBUTE_DARK)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+	Duel.SetChainLimit(aux.FALSE)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,s.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,ft,tp)
+	if Duel.Destroy(g:GetFirst(),REASON_EFFECT)~=0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0xf14,TYPES_TOKEN,1000,1000,8,RACE_FIEND,ATTRIBUTE_DARK) then
+		local token=Duel.CreateToken(tp,id+1)
+		Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
