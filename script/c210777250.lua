34bb784f370e37c4a1c5173f18fe65aaadb6b1c6
Naim <naimsantos45@gmail.com>
2022-06-13 11:10:49 -0300

    Add script for "Pyromania"

--- script/c210777250.lua
+++ script/c210777250.lua
@@ -1,57 +1,45 @@
---Dragon Devouring Virus
+--Pyromania
 --scripted by Naim
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_ACTIVATE)
-	e0:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e0)
-	--All monsters become Dragon
+	--Destroy 1 FIRE monster and add 1 Pyro monster to the hand
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetRange(LOCATION_FZONE)
-	e1:SetTargetRange(LOCATION_MZONE+LOCATION_GRAVE,LOCATION_MZONE+LOCATION_GRAVE)
-	e1:SetCode(EFFECT_CHANGE_RACE)
-	e1:SetValue(RACE_ZOMBIE)
-	e1:SetTarget(s.tg)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimi(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.tg(e,c)
-	if c:GetFlagEffect(1)==0 then
-		c:RegisterFlagEffect(1,0,0,0)
-		local eff
-		if c:IsLocation(LOCATION_MZONE) then
-			eff={Duel.GetPlayerEffect(c:GetControler(),EFFECT_NECRO_VALLEY)}
-		else
-			eff={c:GetCardEffect(EFFECT_NECRO_VALLEY)}
-		end
-		c:ResetFlagEffect(1)
-		for _,te in ipairs(eff) do
-			local op=te:GetOperation()
-			if not op or op(e,c) then return false end
+function s.thfilter(c)
+	return c:IsRace(RACE_PYRO) and c:IsAbleToHand()
+end
+function s.dstfilter(c)
+	return c:IsAttribute(ATTRIBUTE_FIRE) and (c:IsFaceup() or c:IsLocation(LOCATION_HAND))
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.dstfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	local g=Duel.GetMatchingGroup(s.dstfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,1,1-tp,500)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,s.dstfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil)
+	if #g>0 and Duel.Destroy(g,REASON_EFFECT)~=0 then
+		local dg=Duel:GetOperatedGroup():GetFirst()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+			if dg:IsRace(RACE_PYRO) then
+				Duel.BreakEffect()
+				Duel.Damage(1-tp,500,REASON_EFFECT)
+			end
 		end
 	end
-	return true
 end
-
---[[
-Dragon Devouring Virus
-Field Spell Card
-All monsters on the field and in the GYs become Zombie monsters.
-
-Dragon Devouring Plague
-Normal Trap Card
-Destroy all Dragon monsters on the field during the End Phase of this turn.
-You can banish this card from the GY, then target 1 monster in your opponent's GY; Special Summon 1 "Dragon Devouring" monster from your GY with a level lower than that target's level/rank/link and if you do, banish that target. You can only use each effect of "Dragon Devouring Plague" once per turn.
-
-Dragon Devouring Plague
-Normal Trap Card
-Destroy all Dragon monsters on the field during the End Phase of this turn.
-You can banish this card from the GY, then target 1 Dragon in your opponent's GY; Special Summon 1 "Dragon Devouring" monster from your GY with a level lower than that target's level/rank/link and if you do, banish that target. You can only use each effect of "Dragon Devouring Plague" once per turn.
-
-Dragon Devouring Slumber
-Quick Play Spell
-Change all face-up Dragon monsters to face-down defense position, then if there are any face-up Dragon monsters on the field, return them to the hand.
-You can banish this card from the GY, then target 1 Dragon in your opponent's GY; Special Summon 1 "Dragon Devouring" monster from your Deck with a level lower than that target's level/rank/link, but negate its effects, then shuffle that target into the Deck.
-
