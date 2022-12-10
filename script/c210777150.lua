de501c2edb91ab84c12e3f6c39b8b23472872d9d
Naim <naimsantos45@gmail.com>
2022-12-10 05:57:48 -0300

    Update "Specimen Extinction"
    Added a hint message for the tribute part + declare races with constant and not a magic value

--- script/c210777150.lua
+++ script/c210777150.lua
@@ -14,12 +14,13 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
+s.listed_series={SET_FOSSIL}
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(100)
 	return true
 end
 function s.filter1(c)
-	return c:IsSetCard(0x14c) and c:IsType(TYPE_FUSION)
+	return c:IsSetCard(SET_FOSSIL) and c:IsType(TYPE_FUSION)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
@@ -28,11 +29,12 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 		return Duel.CheckReleaseGroupCost(tp,s.filter1,1,false,nil,nil)
 			and Duel.GetFieldGroupCount(tp,0,LOCATION_DECK+LOCATION_EXTRA)>0
 	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
 	local rg=Duel.SelectReleaseGroupCost(tp,s.filter1,1,1,false,nil,nil)
 	e:SetLabel(rg:GetFirst():GetLevel())
 	Duel.Release(rg,REASON_COST)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RACE)
-	local typ=Duel.AnnounceRace(tp,1,0xffff)
+	local typ=Duel.AnnounceRace(tp,1,RACE_ALL)
 	Duel.SetTargetParam(typ)
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,0,LOCATION_DECK+LOCATION_EXTRA)
 end
@@ -40,12 +42,12 @@ function s.tgfilter(c,typ)
 	return c:IsType(TYPE_MONSTER) and c:IsAbleToGrave() and c:IsRace(typ)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local lvl=e:GetLabel()	--lvl
-	local typ=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM) --race
+	local lvl=e:GetLabel()
+	local typ=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
 	local g=Duel.GetMatchingGroup(s.tgfilter,tp,0,LOCATION_DECK+LOCATION_EXTRA,nil,typ)
 	local num=math.min(lvl,#g)
 	local sg=Duel.SelectMatchingCard(1-tp,s.tgfilter,tp,0,LOCATION_DECK+LOCATION_EXTRA,num,num,nil,typ)
-	if sg then
+	if #sg>0 then
 		Duel.SendtoGrave(sg,REASON_RULE)
 	end
 	local og=Duel.GetOperatedGroup()
