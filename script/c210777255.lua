a161aabc59240b08317ce2dcbd980504c8d48e5a
Naim <naimsantos45@gmail.com>
2025-03-21 09:45:56 -0300

    new testes

--- script/c210777255.lua
+++ script/c210777255.lua
@@ -2,5 +2,53 @@
 --scripted by Naim, designed by archimated
 local s,id=GetID()
 function s.initial_effect(c)
-	
+	--Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	--Your Normal monsters cannot be destroyed by battle
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_NORMAL))
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--Reveal 3 "Paleozoic" Trap Cards with different names from your Deck/banishment, then your opponent randomly chooses 1 card to add to your hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_TOGRAVE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+
+end
+s.listed_series={SET_PALEOZOIC}
+function s.thfilter(c)
+	return c:IsSetCard(SET_PALEOZOIC) and (c:IsAbleToHand() or c:IsAbleToGrave()) and (c:IsFaceup() or c:IsLocation(LOCATION_DECK))
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK|LOCATION_REMOVED,0,3,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK|LOCATION_REMOVED,0,nil)
+	if #g>=3 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,3,3,nil)
+		Duel.ConfirmCards(1-tp,sg)
+		Duel.ShuffleDeck(tp)
+		local tg=sg:RandomSelect(1-tp,1)
+		local tc=tg:GetFirst()
+		if tc:IsAbleToHand() then
+			Duel.SendtoHand(tc,nil,REASON_EFFECT)
+			sg:RemoveCard(tc)
+		end
+		Duel.SendtoGrave(sg,REASON_EFFECT)
+	end
 end
